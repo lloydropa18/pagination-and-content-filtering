167013a018cd2ed128a51eed693650a7c3617b68
Lloyd Ropa <lloyd.ropa18@gmail.com>
2020-10-23 18:18:46 +0800

    add function when nothing is found

--- js/app.js
+++ js/app.js
@@ -5,25 +5,43 @@ const searchBtn  = document.getElementById('searchBtn');
 const pages =  document.getElementById('pages');
 const userName = document.querySelectorAll('.userName');
 const students = document.querySelectorAll('.student');
-
+const notFound = document.querySelector('.nothingFound')
 
 // Search Function
 function search(){
+    notFound.style.display = "none";
     const value = searchInput.value;
     const exp = new RegExp(value, 'i'); 
     userName.forEach(name => {
         const match = exp.test(name.innerHTML);
         // If the input does not match add hide class to the element
         if (!match){ 
-            name.parentNode.parentNode.parentNode.parentNode.classList.add('hide');       
+            name.parentNode.parentNode.parentNode.parentNode.classList.add('hide');
         } else if (match){
             name.parentNode.parentNode.parentNode.parentNode.classList.remove('hide');
             if (value === ""){
                 addHideClass('page1');
             }
         }
-    })  
+    });
+    nothingFound();   
 }
+
+
+// Nothing has found search engine
+function nothingFound(){
+    let studentsHide = 0;
+    students.forEach(student=> {
+        if (student.classList.contains('hide')){
+            studentsHide += 1;
+        }
+    });
+    if (studentsHide === students.length){
+        notFound.style.display = "block";
+        console.log(studentsHide)
+    }
+}
+
 
 // Hide the student 
 function addHideClass(page){
